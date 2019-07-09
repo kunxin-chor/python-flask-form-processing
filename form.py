5ac8278ea35039ece26d225cdfca33270e3c2cc7
Ubuntu <ubuntu@ip-172-31-23-186.ap-southeast-1.compute.internal>
2019-07-09 08:29:57 +0000

    Added form example

---
+++ form.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def show_form():
+    return render_template('form.html')
+    
+@app.route('/', methods=['POST'])
+def process_form():
+    print (request.form)
+    name = request.form.get('first_name')
+    message = request.form.get('message')
+    spam = request.form.get("spam")
+    attribution = request.form.get('attribution')
+    if spam == None: 
+        spam = "N/A"
+    accept = request.form.get("accept")
+    if accept == None:
+        return "Please accept our terms and conditions"
+   
+    activities = request.form.getlist('activities[]')
+    print(activities)
+    return "Name={}, message={}, spam={}, attribution={}".format(name, message,spam, attribution)
+
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
